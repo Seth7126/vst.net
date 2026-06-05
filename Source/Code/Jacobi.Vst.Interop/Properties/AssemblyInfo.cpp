0239dca3a16e998cabecfc049e4775a34a0fcc8d
Marc Jacobi <obiwanjacobi@hotmail.com>
2026-06-05 21:37:29 +0200

    Net8 (#75)
    * main proj to net10
    
    * docs
    
    * fixes, deployment and samples
    
    * templates to net10
    
    * extension has changed
    
    * trying to package a deub version for easy testing
    
    * net8
    
    * update version
    
    * more version ref updates
    
    * update version in build target

--- Source/Code/Jacobi.Vst.Interop/Properties/AssemblyInfo.cpp
+++ Source/Code/Jacobi.Vst.Interop/Properties/AssemblyInfo.cpp
@@ -16,12 +16,12 @@ using namespace System::Runtime::InteropServices;
 
 // legal stuff
 [assembly:AssemblyCompany("Jacobi Software")];
-[assembly:AssemblyCopyright("Copyright © 2008-2024 Jacobi Software")] ;
+[assembly:AssemblyCopyright("Copyright © 2008-2026 Jacobi Software")] ;
 [assembly:AssemblyTrademark("obiwanjacobi")] ;
 
-[assembly:AssemblyInformationalVersionAttribute("2.0.3")]
-[assembly:AssemblyVersionAttribute("2.0.3.0")]
-[assembly:AssemblyFileVersionAttribute("2.0.3.0")]
+[assembly:AssemblyInformationalVersionAttribute("2.1.8")]
+[assembly:AssemblyVersionAttribute("2.1.8.0")]
+[assembly:AssemblyFileVersionAttribute("2.1.8.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
